e1e01f730eba86ad6f7f06bad9b5321f1c333757
Anhph-Ccash <anh.pham@c-cashglobal.com>
2025-11-07 12:25:10 +0700

    Remove local secrets and add .gitignore

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = "admin123"
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:11223344@localhost:5432/FlaskWebPostgreSQL"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), "uploads")
-    ALLOWED_EXTENSIONS = {'xls', 'xlsx', 'csv', 'mt940', 'txt', 'zip'}
-    
