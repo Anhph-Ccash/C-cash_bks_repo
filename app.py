3ca42533528124f89650a7c34c55a33e91ab2b7d
Anhph-Ccash <anh.pham@c-cashglobal.com>
2025-11-15 19:47:04 +0700

    Fix: Cập nhật port để tương thích với Render

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     # Allow running via: python app.py run --no-debugger --no-reload
     # We ignore extra CLI args for compatibility with existing launch configs.
     debug = os.getenv("FLASK_ENV", "development") == "development"
-    port = int(os.getenv("PORT", "5001"))
+    port = int(os.getenv("PORT", "5000"))
     app.run(debug=debug, port=port)
